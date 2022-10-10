3f6fe5402d0dbe6b58367388a1a5f7bac89e9a83
Hikmat Jafarli <jafarlihi@gmail.com>
2022-10-10 23:23:06 +0400

    Add stuff

--- parse.c
+++ parse.c
@@ -19,10 +19,48 @@ typedef struct ASTNode {
   struct ASTNode *s3;
 } ASTNode;
 
+ASTNode *makeErr() {
+  ASTNode *err = malloc(sizeof(ASTNode));
+  err->type = ERR;
+  return err;
+}
+
+ASTNode *expression() {
+  ASTNode *expr = malloc(sizeof(ASTNode));
+  Token *token = lex();
+  if (token->kind != INTL)
+    return makeErr();
+  // expr->fields->intval = token->value;
+  return expr;
+}
+
+ASTNode *statement() {
+  ASTNode *stmt = malloc(sizeof(ASTNode));
+  Token *token = lex();
+  if (token->kind != KEYWORD || strcmp(token->lexeme, "return") != 0)
+    return makeErr();
+  stmt->s1 = expression();
+  if (lex()->kind != SEMICOL)
+    return makeErr();
+  return stmt;
+}
+
 ASTNode *function() {
+  ASTNode *func = malloc(sizeof(ASTNode));
   Token *token = lex();
   if (token->kind == KEYWORD && strcmp(token->lexeme, "int") == 0) {
-
+    token = lex();
+    func->fields.strval = token->lexeme;
+    if (token->kind == ID && lex()->kind == OPARAN && lex()->kind == CPARAN && lex()->kind == OBRACE) {
+      ASTNode *stmt = statement();
+      func->s1 = stmt;
+      if (lex()->kind != CBRACE)
+        return makeErr();
+    } else {
+      return makeErr();
+    }
+  } else {
+    return makeErr();
   }
 }
 
